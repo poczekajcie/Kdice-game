f35fc6a7fdf5e857be429ddb706ec5a8ecc78c33
poczekajcie <krakowski.jacek97@wp.pl>
2018-12-16 17:38:20 +0100

    Working with multiple clients

--- src/dev/Kdice/Server.java
+++ src/dev/Kdice/Server.java
@@ -1,18 +1,129 @@
 package dev.Kdice;
 
-import java.net.*;
+// echo server
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Server {
-    public static void main(String[] args) {
-        try
-        {
-            ServerSocket server = new ServerSocket(4444);
-            Socket s = server.accept();
+    public static void main(String args[]){
 
-            System.out.println("Connected!");
-        } catch (Exception e) {
-            System.out.println(e);
-            System.exit(-1);
+
+        Socket s=null;
+        ServerSocket ss2=null;
+        System.out.println("Server Listening......");
+        try{
+            ss2 = new ServerSocket(4445); // can also use static final PORT_NUM , when defined
+
         }
+        catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Server error");
+
+        }
+
+        while(true){
+            try{
+                s= ss2.accept();
+                System.out.println("connection Established");
+                ServerThread st=new ServerThread(s);
+                st.start();
+
+            }
+
+            catch(Exception e){
+                e.printStackTrace();
+                System.out.println("Connection Error");
+
+            }
+        }
+
+    }
+
+}
+
+class ServerThread extends Thread{
+
+    String line=null;
+    BufferedReader  is = null;
+    PrintWriter os=null;
+    Socket s=null;
+
+    public ServerThread(Socket s){
+        this.s=s;
+    }
+
+    public void run() {
+        try{
+            is= new BufferedReader(new InputStreamReader(s.getInputStream()));
+            os=new PrintWriter(s.getOutputStream());
+
+        }catch(IOException e){
+            System.out.println("IO error in server thread");
+        }
+
+        try {
+            line=is.readLine();
+            while(line.compareTo("QUIT")!=0){
+
+                os.println(line);
+                os.flush();
+                System.out.println("Response to Client  :  "+line);
+                line=is.readLine();
+            }
+        } catch (IOException e) {
+
+            line=this.getName(); //reused String line for getting thread name
+            System.out.println("IO Error/ Client "+line+" terminated abruptly");
+        }
+        catch(NullPointerException e){
+            line=this.getName(); //reused String line for getting thread name
+            System.out.println("Client "+line+" Closed");
+        }
+
+        finally{
+            try{
+                System.out.println("Connection Closing..");
+                if (is!=null){
+                    is.close();
+                    System.out.println(" Socket Input Stream Closed");
+                }
+
+                if(os!=null){
+                    os.close();
+                    System.out.println("Socket Out Closed");
+                }
+                if (s!=null){
+                    s.close();
+                    System.out.println("Socket Closed");
+                }
+
+            }
+            catch(IOException ie){
+                System.out.println("Socket Close Error");
+            }
+        }//end finally
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+The code is based on https://stackoverflow.com/questions/10131377/socket-programming-multiple-client-to-one-server
+*/
